cde18ad11cf40cbce4db109ae0f1b5bf54275fb5
ajaythoram <115677014+ajaythoram@users.noreply.github.com>
2023-05-15 15:51:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+const evaluatedText = document.getElementById("evaluatedText");
+const letterCount = document.getElementById("letterCount");
+
+evaluatedText.addEventListener("input", function() {
+  const inputText = this.value;
+  const count = inputText.length;
+  letterCount.textContent = count;
+});
+
